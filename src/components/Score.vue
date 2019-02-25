f4892bc4c084c0cc6053395a9d79c34832f58079
Fethi <fchouder@largus.fr>
2019-02-25 15:44:56 +0100

    initial commit

---
+++ src/components/Score.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="score" v-if="player">
+        {{ score }}
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'score',
+  props: ['score', 'player']
+}
+</script>
+
+<style scoped>
+    .score {
+        display: inline-block;
+        height: 50px;
+        width: 150px;
+        font-size: 17pt;
+        font-weight: 500;
+        color: greenyellow;
+        background-color: #666;
+        line-height: 50px;
+        padding: 5px;
+        margin: 0 auto;
+    }
+</style>
